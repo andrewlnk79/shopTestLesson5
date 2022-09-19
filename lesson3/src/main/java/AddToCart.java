e30549bc2d2ede90e112fd4978823346124ed68c
AndreyLinnik <av.linnik@ya.ru>
2022-09-19 12:09:10 +0300

    update

--- lesson3/src/main/java/AddToCart.java
+++ lesson3/src/main/java/AddToCart.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -14,6 +15,9 @@ public class AddToCart {
 
         WebDriver driver= new ChromeDriver(options);
         driver.get("http://automationpractice.com");
+        WebElement element1= driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]/div/div[1]/div/a[1]/img"));
+element1.click();
+
 
 
 
