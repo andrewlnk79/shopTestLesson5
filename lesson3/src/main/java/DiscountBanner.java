38dc95a5ffce2eea93ecc893f33d5566c30eb67c
AndreyLinnik <av.linnik@ya.ru>
2022-09-19 11:54:01 +0300

    -update

--- lesson3/src/main/java/DiscountBanner.java
+++ lesson3/src/main/java/DiscountBanner.java
@@ -1,13 +1,18 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DiscountBanner {
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
+        ChromeOptions options= new ChromeOptions();
         options.addArguments("--incognito");
         options.addArguments("start-maximized");
+
+        WebDriver driver= new ChromeDriver(options);
+        driver.get("http://automationpractice.com");
 
     }
 
