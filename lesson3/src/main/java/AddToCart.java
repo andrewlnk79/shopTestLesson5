824d2266a4d9e23c3b8eebda5ae3d0f493fc356f
AndreyLinnik <av.linnik@ya.ru>
2022-09-19 11:52:52 +0300

    onChrome

--- lesson3/src/main/java/AddToCart.java
+++ lesson3/src/main/java/AddToCart.java
@@ -1,22 +1,21 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 public class AddToCart {
     public static void main(String[] args) {
-
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
+        ChromeOptions options= new ChromeOptions();
         options.addArguments("--incognito");
         options.addArguments("start-maximized");
-        WebDriver driver = new ChromeDriver(options);
-        //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("http://automationpractice.com/index.php?id_product=1&controller=product");
-        WebElement addToCart = driver.findElement(By.xpath("//*[@id=\"add_to_cart\"]/button/span"));
-        addToCart.click();
+
+        WebDriver driver= new ChromeDriver(options);
+        driver.get("http://automationpractice.com");
+
+
 
 
 
